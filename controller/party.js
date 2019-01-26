942c8ac4e660916388097c885618e9389dbe0196
Tijani Femi <40898511+femitj@users.noreply.github.com>
2019-01-26 22:07:09 +0100

    up2 (#9)
    love

--- controller/party.js
+++ controller/party.js
@@ -1,7 +1,7 @@
 import db from '../models/politicaloffice';
 
 class OfficeController {
-
+ 
   static createOffice(req, res) {
     const office = {
       id: db.length + 1,
@@ -10,7 +10,18 @@ class OfficeController {
       createdOn: new Date().toDateString(),
     };
 
-
+    db.push(office);
+    // political office created
+    return res.status(201).json({
+      status: 201,
+      message: 'Political office created',
+      data: [{
+        id: office.id,
+        type: office.type,
+        name: office.name,
+      }],
+    });
+  }
 }
 
 export default OfficeController;
