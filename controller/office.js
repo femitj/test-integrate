7b8410ea7cf5c166167f736eb36a2083c5e10654
Tijani Femi <40898511+femitj@users.noreply.github.com>
2019-01-26 22:14:58 +0100

    see (#10)

--- controller/office.js
+++ controller/office.js
@@ -2,7 +2,13 @@ import db from '../models/politicaloffice';
 
 class OfficeController {
  
-
+  static createOffice(req, res) {
+    const office = {
+      id: db.length + 1,
+      type: req.body.type, // String
+      name: req.body.name, // // String
+      createdOn: new Date().toDateString(),
+    };
 
     db.push(office);
     // political office created
