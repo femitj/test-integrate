2ab677928fcdbd02da662ec077313fff850b93f9
Tijani Oluwafemi <olutimedia@gmail.com>
2019-01-26 22:09:38 +0100

    see

---
+++ controller/office.js
@@ -0,0 +1,16 @@
+import db from '../models/politicaloffice';
+
+class OfficeController {
+ 
+  static createOffice(req, res) {
+    const office = {
+      id: db.length + 1,
+      type: req.body.type, // String
+      name: req.body.name, // // String
+      createdOn: new Date().toDateString(),
+    };
+
+
+}
+
+export default OfficeController;
