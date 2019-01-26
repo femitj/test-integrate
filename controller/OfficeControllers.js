1c20895b27a17c73600120b09b6ddafa833c68ee
Tijani Oluwafemi <olutimedia@gmail.com>
2019-01-26 21:48:14 +0100

    try 2

---
+++ controller/OfficeControllers.js
@@ -0,0 +1,20 @@
+import db from '../models/politicaloffice';
+
+class OfficeController {
+
+    db.push(office);
+    // political office created
+    return res.status(201).json({
+      status: 201,
+      message: 'Political office created',
+      data: [{
+        id: office.id,
+        type: office.type,
+        name: office.name,
+      }],
+    });
+  }
+
+}
+
+export default OfficeController;
