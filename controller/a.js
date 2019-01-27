cf14fef22570415a88a3ca53aa86c54bb6f965dc
Tijani Oluwafemi <olutimedia@gmail.com>
2019-01-27 07:04:54 +0100

    done

---
+++ controller/a.js
@@ -0,0 +1,27 @@
+import db from '../models/politicaloffice';
+
+class OfficeController {
+ 
+  static createOffice(req, res) {
+    const office = {
+      id: db.length + 1,
+      type: req.body.type, // String
+      name: req.body.name, // // String
+      createdOn: new Date().toDateString(),
+    };
+
+    db.push(office);
+    // political office created
+    return res.status(201).json({
+      status: 201,
+      message: 'Political office created',
+      data: [{
+        id: office.id,
+        type: office.type,
+        name: office.name,
+      }],
+    });
+  }
+}
+
+export default OfficeController;
